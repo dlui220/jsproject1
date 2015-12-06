539464a8084c527927997596120ca04f2d774ce8
Derrick Lui <derricklui1@gmail.com>
2015-12-06 01:04:23 -0500

    worked on JS list

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ window.onload = function() {
         return false;
     }
 };
+
+var addItem = function addItem(item,list){
+		var youngkim = document.createElement("li");
+		youngkim.innerHTML = "<a>" + item + "</a>";
+		list.appendChild(youngkim);
+};
+
+var itemCallback = function itemCallback(e){
+		var urllist = document.getElementById("urllist");
+		urllist.appendChild(this);
+};
+
+var buttonCallback = function buttonCallback(e){
+		var list = document.getElementById("urllist");
+		var textbox = document.getElementById("textbox");
+		var input = textbox.value;
+		if (input != ""){
+				addItem(input,list);
+				list.children[list.children.length - 1].addEventListener("click",itemCallback);
+		}
+		textbox.value = "";
+};
+
+var button = document.getElementById("button");
+button.addEventListener("click",buttonCallback);
+
+
+
